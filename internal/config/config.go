edd34c5920bd41287d7f43418e2a84a90382e765
Mikhail Svistulenko <djthor@bk.ru>
2024-07-18 20:50:30 +0300

    Iteration 13: Still config fixes

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,40 +1,15 @@
 package config
 
 import (
-	"fmt"
-
 	"github.com/caarlos0/env"
 	flag "github.com/spf13/pflag"
 )
 
-type Address string
-
 type Config struct {
-	ServerAddress   Address `env:"SERVER_ADDRESS"`
-	BaseURL         Address `env:"BASE_URL"`
-	FileStoragePath string  `env:"FILE_STORAGE_PATH"  envDefault:"/tmp/short-url-db.json"`
-	DatabaseDSN     string  `env:"DATABASE_DSN"`
-}
-
-func (a *Address) Set(flagValue string) error {
-	*a = Address(flagValue)
-	return nil
-}
-
-func (a *Address) String() string {
-	return string(*a)
-}
-
-func (a *Address) Type() string {
-	return "string"
-}
-
-func (a *Address) UnmarshalText(envValue []byte) error {
-	if len(envValue) == 0 {
-		return fmt.Errorf("cannot be empty")
-	}
-	*a = Address(string(envValue))
-	return nil
+	ServerAddress   string `env:"SERVER_ADDRESS"`
+	BaseURL         string `env:"BASE_URL"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"  envDefault:"/tmp/short-url-db.json"`
+	DatabaseDSN     string `env:"DATABASE_DSN"`
 }
 
 func NewConfig() *Config {
@@ -52,8 +27,8 @@ func NewConfig() *Config {
 }
 
 func parseFlags(c *Config) {
-	flag.VarP(&c.ServerAddress, "address", "a", "address of shortener service server")
-	flag.VarP(&c.BaseURL, "basepath", "b", "address of short link basepath")
+	flag.StringVarP(&c.ServerAddress, "address", "a", "", "address of shortener service server")
+	flag.StringVarP(&c.BaseURL, "basepath", "b", "", "address of short link basepath")
 	flag.StringVarP(&c.FileStoragePath, "file_storage_path", "f", "", "path to file storage of URLs")
 	flag.StringVarP(&c.DatabaseDSN, "database_dsn", "d", "", "db connection string")
 	flag.Parse()
