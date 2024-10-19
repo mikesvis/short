7f1b6a3b0a86d7bd733e019dc4740b3e4dc1e264
Mikhail Svistulenko <djthor@bk.ru>
2024-10-19 18:22:46 +0300

    Iter23: Postgres test test testing conn

---
+++ internal/drivers/postgres/postgres_test.go
@@ -0,0 +1,41 @@
+// Модуль storage для хранения в базе.
+package postgres
+
+import (
+	"testing"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/mikesvis/short/internal/logger"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewPostgres(t *testing.T) {
+	l, _ := logger.NewLogger()
+	type args struct {
+		databaseDSN string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Postgres
+	}{
+		{
+			name: "New storage is of type",
+			args: args{
+				databaseDSN: "postgres://postgres:postgres@127.0.0.1:5432/praktikum?sslmode=disable",
+			},
+			want: &Postgres{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db, err := sqlx.Open("postgres", tt.args.databaseDSN)
+			require.NoError(t, err)
+			newStorage, err := NewPostgres(db, l)
+			require.NoError(t, err)
+			assert.IsType(t, tt.want, newStorage)
+		})
+	}
+}
